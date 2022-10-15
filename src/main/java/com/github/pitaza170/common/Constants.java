9a84bff666a861013aa217d7f97609f880f5a2c1
Artur <arturbagaev90@gmail.com>
2022-10-15 20:21:13 +0300

    refactor: add ControllerAdvice to the project

--- src/main/java/com/github/pitaza170/common/Constants.java
+++ src/main/java/com/github/pitaza170/common/Constants.java
@@ -2,5 +2,8 @@ package com.github.pitaza170.common;
 
 @SuppressWarnings("java:S1118")
 public class Constants {
+    public static final String TRACE = "trace";
     public static final String FAILED_TO_PARSE="Failed to parse csv file: ";
+    public static final String VALIDATION_ERROR = "Validation error. Check 'error' field for details";
+    public static final String ENTITY_NOT_FOUND = "Entity not found";
 }
