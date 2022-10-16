01e389a56be9646669f331d0054f2f04cf1367c5
Artur <arturbagaev90@gmail.com>
2022-10-16 17:11:18 +0300

    minor: add some common constants

--- src/main/java/com/github/pitaza170/common/Constants.java
+++ src/main/java/com/github/pitaza170/common/Constants.java
@@ -6,4 +6,7 @@ public class Constants {
     public static final String FAILED_TO_PARSE="Failed to parse csv file: ";
     public static final String VALIDATION_ERROR = "Validation error. Check 'error' field for details";
     public static final String ENTITY_NOT_FOUND = "Entity not found";
+    public static final String SUCCESS = "Success";
+    public static final String SUCCESSFULLY_DELETED = "Successfully deleted";
+    public static final String SUCCESSFULLY_UPLOADED = "Uploaded the file successfully:";
 }
